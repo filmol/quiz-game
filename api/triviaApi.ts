c81ba89e9ecdc8bcf79a914ac2ac4cd95a92762f
filmol <40041318+filmol@users.noreply.github.com>
2022-05-23 10:32:08 +0200

    Add async fetch function for trivia API

---
+++ api/triviaApi.ts
@@ -0,0 +1,24 @@
+/**
+ * A GET requests to opentdb API endpoint
+ * @returns Parsed API call response OR null if failed
+ */
+export async function fetchQuestions() {
+  const response = await fetch(
+    `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`
+  );
+
+  // Handle opentdb API response
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`An error occured please try again`);
+  }
+  try {
+    const data: any = await response.json();
+    console.log(data.results);
+    return data.results;
+  } catch (err) {
+    console.error(response.statusText);
+    console.log('API fetch for Strapi News failed with: ' + err);
+    return null;
+  }
+}
